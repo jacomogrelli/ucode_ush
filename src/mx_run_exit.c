da7b4fbc722d53d332049b72f21ed5ae59360290
jacomogrelli <nechaeff@gmail.com>
2020-03-01 13:41:26 +0200

    builtin exit is done

--- src/mx_run_exit.c
+++ src/mx_run_exit.c
@@ -19,6 +19,29 @@ static int get_exit_status(char **com, int count, bool *err) {
         return 1;
 }
 
+static void exit_err(char *com, int flag) {
+    switch (flag) {
+        case 1:
+            if (isatty(0))
+                mx_printstr("exit\n");
+            mx_printerr("ush: ");
+            if (!isatty(0))
+                mx_printerr("line 1: ");
+            mx_printerr("exit: too many arguments\n");
+            break;
+        default:
+            if (isatty(0))
+                mx_printstr("exit\n");
+            mx_printerr("ush: ");
+            if (!isatty(0))
+                mx_printerr("line 1: ");
+            mx_printerr("exit: ");
+            mx_printerr(com);
+            mx_printerr(": numeric argument required\n");
+            break;
+    }
+}
+
 void mx_run_exit(t_envp *var, char **command) {
     bool err = false;
     int count = 0;
@@ -26,47 +49,15 @@ void mx_run_exit(t_envp *var, char **command) {
      while (command[count])
         count++;
     int i = get_exit_status(command, count, &err);
-    if (isatty(0))
-    if (count != 1 && err) {
+    if (count != 1 && err && i != -1) {
         mx_envp_replace(&var, "?=1");
+        exit_err(command[1], 1);
         return;
     }
+    if (i == -1 && err) {
+        exit_err(command[1], 2);
+        exit (-1);
+    }
+    mx_printstr("exit\n");
     exit (i);
 }
-
-/*
-bash-3.2$ exit 0 asdf 	                    // doesn’t exit
-exit
-bash: exit: too many arguments
-bash-3.2$ echo $?
-1
-
-bash-3.2$ exit 10 10                        // doesn’t exit
-exit
-bash: exit: too many arguments
-bash-3.2$ echo $?
-1
-
-bash-3.2$ exit
-exit
-➜  ~ echo $?
-0
-
-bash-3.2$ exit /asdf
-exit
-bash: exit: /asdf: numeric argument required       //exit with -1
-➜  ~ echo $?
-255
-
-bash-3.2$ exit asdf asdf
-exit
-bash: exit: asdf: numeric argument required      //exit with -1
-➜  ~ echo $?
-255
-
-bash-3.2$ exit asdf 10
-exit
-bash: exit: asdf: numeric argument required
-➜  ~ echo $?
-255
- */
