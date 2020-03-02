87505e4f6420b29ce81bc1051bdd52357263ff4b
Yaroslav Pratsovity <ypratsovit@e2r4p4.unit.ua>
2020-03-02 15:34:16 +0200

    fixed_some_bugs_cd

--- src/mx_cd_without_flags.c
+++ src/mx_cd_without_flags.c
@@ -1,11 +1,14 @@
 #include "ush.h"
 
 static void cd_with_one_arg(char **splited_arg, t_dirs_cd *dirs_cd) {
+        struct stat buf;
+
+        lstat(splited_arg[0], &buf);
         chdir(splited_arg[0]);
         setenv("OLDPWD", dirs_cd->get_pwd, 1);
         setenv("PWD", splited_arg[0], 1);
-        printf("%s ", getenv("PWD"));
-        mx_set_correct_path();
+        if (!(S_ISLNK(buf.st_mode)))
+            mx_set_correct_path();
 }
 
 void mx_cd_without_flags(char **splited_arg, t_dirs_cd *dirs_cd) {
