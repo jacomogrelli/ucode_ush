80291e6edc8ec7284f2599b97ea6fab6fad01df8
Yaroslav Pratsovity <ypratsovit@e2r4p4.unit.ua>
2020-03-04 16:48:08 +0200

    add_whitespases_e_echo

--- src/mx_print_e_for_echo.c
+++ src/mx_print_e_for_echo.c
@@ -8,10 +8,42 @@ void mx_print_e_for_echo(char *str) {
             case 'n': {
                 printf("%c", '\n');
                 i++;
+                break;
+            }
+            case 'a': {
+                printf("%c", '\a');
+                i++;
+                break;
+            }
+            case 'v': {
+                printf("%c", '\v');
+                i++;
+                break;
+            }
+            case 't': {
+                printf("%c", '\t');
+                i++;
+                break;
+            }
+            case 'b': {
+                printf("%c", '\b');
+                i++;
+                break;
+            }
+            case 'f': {
+                printf("%c", '\f');
+                i++;
+                break;
+            }
+            case 'r': {
+                printf("%c", '\f');
+                i++;
+                break;
             }
             default: {
                 i++;
                 printf("%c", str[i]);
+                break;
             }
             break;
             }
