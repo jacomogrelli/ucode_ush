b4dcaf16abe49bdf340ba9f2ac1e2337ef5a502e
jacomogrelli <nechaeff@gmail.com>
2020-05-01 12:55:31 +0300

    fixed command env with flags -i and -u

--- src/mx_env_main.c
+++ src/mx_env_main.c
@@ -1,5 +1,26 @@
 #include "ush.h"
 
+static void get_command(t_envp *var, char **com) {
+    char *bin[] = {"env", "export", "set", "unset", "pwd", "exit", "cd",
+                   "which", "echo", "true", "false", "help", "return",
+                   "chdir", "apropos", "arch", NULL};
+    void (*bin_f[])() = {&mx_env_run, &mx_export_run, &mx_set_run,
+                         &mx_unset_run, &mx_pwd_run, &mx_run_exit,
+                         &mx_run_cd_commnd, &mx_which_run,
+                         &mx_run_echo_command, &mx_call_true, &mx_call_false,
+                         &mx_help_command, &mx_return_command,
+                         &mx_chdir_command, &mx_apropos_command,
+                         &mx_arch_command};
+
+    for (int i = 0; bin[i]; i++)
+        if (!strcmp(com[0], bin[i])) {
+            (*bin_f[i])(var, com);
+            return;
+        }
+    var = mx_envp_i_fill();
+    mx_run_exec(com, var);
+}
+
 void mx_env_modechoose(t_envp *var, char **com,int *pos) {
     int com_count = 0;
     char **com_new = NULL;
@@ -15,7 +36,7 @@ void mx_env_modechoose(t_envp *var, char **com,int *pos) {
     for (int i = 0; i < com_count; i++, (*pos)++)
         com_new[i] = strdup(com[(*pos)]);
     com_new[com_count] = NULL;
-    mx_get_command(var, com_new);
+    get_command(var, com_new);
 }
 
 void mx_env_main_empty(void) {
